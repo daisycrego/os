e4c5b601f5e8f6be1d4774d33750f507dab48ca3
Daisy Crego <daisycrego@gmail.com>
2019-02-18 10:45:47 -0500

    Separated execution back into functions - no difference, same issues.

--- hw1/head.c
+++ hw1/head.c
@@ -1,67 +1,6 @@
 #include "types.h"
 #include "user.h"
 
-int main(int argc, char *argv[]){
-
-  int bufCap = 512;
-  int bufSize = 0;
-  char* buf = malloc(bufCap);
-  int numOflines = 10;
-	int fd;
-
-  if (argc <= 1){ //filename NOT provided
-    fd = 0;
-  }
-  else { //filename provided
-    if((fd = open(argv[1], 0)) < 0){
-      printf(1, "head: cannot open %s\n", argv[1]);
-      exit();
-    }
-  }
-
-  char temp;
-
-  //int n = -1;
-  int l = 0;
-
-  int n = read(fd, &temp, 1);
-  while(n > 0 && l < numOflines){
-    //printf(1, "temp: %c\n", temp);
-    if (temp == '\n'){
-      l++;
-      //printf(1, "incrementing l: %d\n", l);
-    }
-    buf[bufSize] = temp;
-    bufSize++;
-    if (bufSize == bufCap){
-      //printf(1, "resizing buffer\n");
-      char* outArr = malloc(bufSize*2);
-      int j;
-      for (j = 0; j < bufSize; j++)
-        outArr[j] = buf[j];
-      free(buf);
-      buf = outArr;
-      bufCap *= 2;
-    }
-
-    n = read(fd, &temp, 1);
-  }
-
-  //printf(1, "start printing... \n");
-  //printf(1, "bufSize: %d\n", bufSize);
-  int i;
-  for(i = 0; i < bufSize; i++){
-    printf(1, "%c", buf[i]);
-  }
-  //printf(1, "done printing... \n");
-
-  //printf(1, "free buf\n");
-  free(buf);
-  exit();
-}
-
-/*
-
 char* resize(char* inArr, int* size){
     char* outArr = malloc(*(size)*2);
     int i;
@@ -83,7 +22,7 @@ void printArr(char* arr, int size){
 void readHead(int fd, int numOflines, char* buf, int bufSize, int bufCap){
   char temp;
 
-  int n = -1;
+  int n;
   int l = 0;
 
   while(((n = read(fd, &temp, 1)) > 0) && l < numOflines){
@@ -97,4 +36,36 @@ void readHead(int fd, int numOflines, char* buf, int bufSize, int bufCap){
   }
   return;
 }
-*/
+
+int main(int argc, char *argv[]){
+
+  int bufCap = 512;
+  int bufSize = 0;
+  char* buf = malloc(bufCap);
+  int numOflines = 10;
+	int fd;
+  char temp;
+  int l = 0;
+  int n;
+
+  if (argc <= 1){ //filename NOT provided
+    fd = 0;
+  }
+  else { //filename provided
+    if((fd = open(argv[1], 0)) < 0){
+      printf(1, "head: cannot open %s\n", argv[1]);
+      exit();
+    }
+  }
+
+  readHead(fd, numOflines, buf, bufSize, bufCap);
+
+  //printf(1, "start printing... \n");
+  //printf(1, "bufSize: %d\n", bufSize);
+  printArr(buf, bufSize);
+  //printf(1, "done printing... \n");
+
+  //printf(1, "free buf\n");
+  free(buf);
+  exit();
+}
